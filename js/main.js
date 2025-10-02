6bbf4b06d6ac58d95bdaa6c171576f90c9fd674f
quoc-quy <quyleom10@gmail.com>
2025-10-02 16:23:41 +0700

    Làm phần Stats

--- js/main.js
+++ js/main.js
@@ -55,3 +55,28 @@ $(function () {
         dots: true,
     });
 });
+
+/* ##############################
+        Stats
+############################### */
+$(function () {
+    const counterUp = window.counterUp.default;
+
+    const callback = (entries) => {
+        entries.forEach((entry) => {
+            const el = entry.target;
+            if (entry.isIntersecting && !el.classList.contains("is-visible")) {
+                counterUp(el, {
+                    duration: 2000,
+                    delay: 16,
+                });
+                el.classList.add("is-visible");
+            }
+        });
+    };
+
+    const IO = new IntersectionObserver(callback, { threshold: 1 });
+
+    const el = document.querySelectorAll(".counter").forEach((node) => IO.observe(node));
+    IO.observe(el);
+});
